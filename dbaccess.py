553a810e2e1ed53ffb96e827fb192da7908932b5
andi <andferdd@citromail.hu>
2022-03-08 15:40:15 +0100

    Ready for testing 3

--- dbaccess.py
+++ dbaccess.py
@@ -5,7 +5,7 @@
 #This document is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License, version 3, 
 #as published by the Free Software Foundation. 
 
-################################
+###############################
 
 from distutils.command.check import check
 
--- validators.py
+++ validators.py
@@ -5,7 +5,7 @@
 #This document is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License, version 3, 
 #as published by the Free Software Foundation. 
 
-################################
+############################### 
 
 import re
 import datetime
